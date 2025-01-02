08ec25793ec1bd42067edb9c27453616ff507322
Seb Wiechers <sebastiaanwiechers@icloud.com>
2025-01-02 16:16:13 +0100

    add basic aliases to zshrc

--- provision.sh
+++ provision.sh
@@ -1,2 +1,11 @@
+# use zsh
 apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# add default aliases
+cat <<EOF >> ~/.zshrc 
+alias ll='ls -laF'
+alias code='code-insiders'
+EOF
+
+
