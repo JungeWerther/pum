278784730159aae399dced576faf40fe5982acf6
Seb Wiechers <sebastiaanwiechers@icloud.com>
2025-01-31 14:19:00 +0100

    added pass

--- provision.sh
+++ provision.sh
@@ -1,11 +1,10 @@
-# use zsh
-apt install zsh
+# use some basics
+apt install zsh tree pass
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # add default aliases
-cat <<EOF >> ~/.zshrc 
+cat <<EOF >> ~/.zshrc
 alias ll='ls -laF'
 alias code='code-insiders'
 EOF
-
-
